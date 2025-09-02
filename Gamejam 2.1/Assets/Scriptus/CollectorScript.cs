632b3a2a91f184de20936ad2a2a83da0b6ff7586
= <38752@ma-web.nl>
2025-09-02 12:25:02 +0200

    one more change to coin collect

--- Gamejam 2.1/Assets/Scriptus/CollectorScript.cs
+++ Gamejam 2.1/Assets/Scriptus/CollectorScript.cs
@@ -2,11 +2,13 @@ using UnityEngine;
 
 public class CollectorScript : MonoBehaviour
 {
+    [SerializeField] private PointsManagerScript pointsManager;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            pointsManager.manaPoints += 1;
             Destroy(other.gameObject);
         }
     }
